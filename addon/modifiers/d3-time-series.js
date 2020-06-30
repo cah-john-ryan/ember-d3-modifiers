6f73cbc576b1091fb9faba3bce91bcc35b84dacb
John Ryan <john.ryan@cardinalhealth.com>
2020-06-30 15:17:08 -0400

    basic setup

--- addon/modifiers/d3-time-series.js
+++ addon/modifiers/d3-time-series.js
@@ -1,7 +1,141 @@
 import Modifier from 'ember-modifier';
+import { tracked } from "@glimmer/tracking";
+import * as d3 from 'd3';
 
 export default class D3TimeSeriesModifier extends Modifier {
   didReceiveArguments() {
-    console.log(`didReceiveArguments running: ${this.element}`)
+    this.loadD3Chart();
+  }
+
+  get height() {
+    return this.d3Config.height - this.d3Config.margin.top - this.d3Config.margin.bottom;
+  }
+  get width() {
+    return this.d3Config.width - this.d3Config.margin.left - this.d3Config.margin.right;
+  }
+
+  get d3Config() {
+    return this.args.named.d3Config;
+  }
+
+  @tracked
+  chartData = this.args.named.chartData;
+
+  loadD3Chart() {
+    const svg = this.createSvg();
+
+    const thresholdSeriesData = this.thresholdLines;
+
+    const xScale = this.xScaleGenerator([].concat(this.chartData, ...thresholdSeriesData));
+    const yScale = this.yScaleGenerator([].concat(this.chartData, ...thresholdSeriesData));
+    this.createXandYaxis(svg, xScale, yScale);
+
+    this.args.named.renderData(svg, xScale, yScale, this.d3Config, this.chartData);
+
+    this.renderThresholdLines(svg, xScale, yScale, thresholdSeriesData);
+
+    this.renderAxisLabels(svg);
+  }
+
+  createSvg() {
+    return d3.select(this.element)
+      .append('svg')
+      .attr('width', this.d3Config.width)
+      .attr('height', this.d3Config.height);
+  }
+
+  get thresholdLines() {
+    const { minDate, maxDate } = this.chartData.reduce((currentValues, temperatureReading) => {
+      currentValues.minDate = currentValues.minDate > temperatureReading.date ? temperatureReading.date : currentValues.minDate;
+      currentValues.maxDate = currentValues.maxDate < temperatureReading.date ? temperatureReading.date : currentValues.maxDate;
+      return currentValues;
+    }, { minDate: this.chartData[0].date, maxDate: this.chartData[0].date });
+
+    let thresholdSeriesData = [];
+    if (this.d3Config.thresholds && this.d3Config.thresholds.length > 0) {
+      this.d3Config.thresholds.forEach(threshold => {
+        let thresholdData =
+          [
+            {
+              seriesId: threshold.thresholdId,
+              date: minDate,
+              value: threshold.value
+            },
+            {
+              seriesId: threshold.thresholdId,
+              date: maxDate,
+              value: threshold.value
+            }
+          ];
+        thresholdSeriesData.push(thresholdData);
+      })
+    }
+    return thresholdSeriesData;
+  }
+
+  xScaleGenerator(dataSeries) {
+    return d3.scaleTime()
+      .domain(d3.extent(dataSeries, d => d.date))
+      .range([this.d3Config.margin.left, this.d3Config.width - this.d3Config.margin.right]);
+  }
+
+  yScaleGenerator(dataSeries) {
+    return d3.scaleLinear()
+      .domain(this.d3Config.startYaxisAtZero ? [0, d3.max(dataSeries, d => d.value)] : d3.extent(dataSeries, d => d.value))
+      .range([this.d3Config.height - this.d3Config.margin.bottom, this.d3Config.margin.top]);
+  }
+
+  createXandYaxis(svg, xScale, yScale) {
+    svg.append('g').call((g) => this.xAxis(g, xScale));
+    svg.append('g').call((g) => this.yAxis(g, yScale));
+  }
+
+  xAxis(g, xScale) {
+    return g.attr('transform', `translate(0,${this.d3Config.height - this.d3Config.margin.bottom})`).call(
+      d3
+        .axisBottom(xScale)
+        .ticks(this.d3Config.axis.x.tickCount)
+        .tickSizeOuter(0)
+    );
+  }
+
+  yAxis(g, yScale) {
+    return g.attr('transform', `translate(${this.d3Config.margin.left},0)`).call(
+      d3
+        .axisLeft(yScale)
+        .ticks(this.d3Config.axis.y.tickCount)
+        .tickSizeOuter(0)
+    );
+  }
+
+  renderThresholdLines(svg, xScale, yScale, thresholdSeriesData) {
+    var valueline = d3.line()
+      .x(function (d) { return xScale(d.date); })
+      .y(function (d) { return yScale(d.value); });
+
+    if (thresholdSeriesData.length > 0) {
+      thresholdSeriesData.forEach(thresholdData => {
+        const thresholdDataSeriesIdDashCased = thresholdData[0].seriesId.replace(/\s+/g, '-').toLowerCase();
+        svg.append('path')
+          .datum(thresholdData)
+          .attr('class', thresholdDataSeriesIdDashCased)
+          .attr('d', valueline);
+      })
+    }
+  }
+
+  renderAxisLabels(svg) {
+    svg.append('text')
+      .attr('text-anchor', 'end')
+      .attr('x', this.d3Config.margin.left + this.width / 2)
+      .attr('y', this.height + this.d3Config.margin.top + this.d3Config.margin.bottom * 0.8)
+      .text(this.d3Config.axis.x.title);
+
+    svg.append('text')
+      .attr('text-anchor', 'end')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', this.d3Config.margin.left * 0.4)
+      .attr('x', -this.height / 2 + this.d3Config.margin.bottom)
+      .text(this.d3Config.axis.y.title);
   }
 }
--- tests/dummy/app/components/d3-time-series.js
+++ tests/dummy/app/components/d3-time-series.js
@@ -1,4 +1,81 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import * as moment from 'moment';
 
 export default class D3TimeSeriesComponent extends Component {
+  d3Config = {
+    height: 400,
+    width: 900,
+    margin: { top: 30, right: 30, bottom: 50, left: 50 },
+    axis: {
+      x: {
+        title: 'Date',
+        tickCount: 16
+      },
+      y: {
+        title: 'Temperature (°C)',
+        tickCount: 5,
+      }
+    },
+    elementSize: 3,
+    thresholds: [
+      { thresholdId: 'High Temperature Value', value: 6 },
+      { thresholdId: 'Low Temperature Value', value: 2 }
+    ]
+  };
+
+  fakeSeriesIds = ['TemperatureA', 'TemperatureB'];
+
+  get chartData() {
+    let chartDataToRender = [];
+    const seedTime = moment().add(-48, 'hour');
+    this.fakeSeriesIds.forEach(fakeSeriesId => {
+      let fakeTimeSeries = this.generateFakeTimeSeries(fakeSeriesId, seedTime.clone());
+      chartDataToRender.push(...fakeTimeSeries);
+    });
+    return chartDataToRender;
+  }
+
+  generateFakeTimeSeries(fakeSeriesId, seedTime) {
+    let data = [];
+    let d = seedTime;
+    for (let i = 0, v = 4; i < 48; ++i) {
+      v += Math.random() - 0.5;
+      v = i === 0 ? 4 : Math.max(Math.min(v, 8), 0);
+      data.push({
+        seriesId: fakeSeriesId,
+        date: d.toDate(),
+        value: v
+      });
+
+      d = d.add(1, 'hour');
+    }
+    return data;
+  }
+
+  @action
+  renderData(svg, xScale, yScale, d3Config, dataToRender) {
+    let seriesNumber = 1;
+    this.getSeriesIdListing(dataToRender).forEach(seriesId => {
+      const seriesIdDashCased = seriesId.replace(/\s+/g, '-').toLowerCase();
+      const seriesData = dataToRender.filter(d => d.seriesId === seriesId);
+      svg.selectAll('whatever')
+        .data(seriesData)
+        .enter()
+        .append('circle')
+        .attr('cx', d => xScale(d.date))
+        .attr('cy', d => yScale(d.value))
+        .attr('r', d3Config.elementSize)
+        .attr('class', `dot series-${seriesNumber++} ${seriesIdDashCased}`);
+    });
+  }
+
+  getSeriesIdListing(dataToRender) {
+    return dataToRender.reduce((listing, temperatureReading) => {
+      if (!listing.includes(temperatureReading.seriesId)) {
+        listing.push(temperatureReading.seriesId);
+      }
+      return listing;
+    }, []);
+  }
 }
