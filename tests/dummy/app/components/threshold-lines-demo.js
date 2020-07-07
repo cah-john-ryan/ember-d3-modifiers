965f81015a728fa2e3a4e55d51b659063865ace3
John Ryan <john.ryan@cardinalhealth.com>
2020-07-07 11:41:13 -0400

    Adjustment to the threshold lines demo

--- tests/dummy/app/components/threshold-lines-demo.js
+++ tests/dummy/app/components/threshold-lines-demo.js
@@ -1,21 +1,30 @@
 import Component from '@glimmer/component';
 import { D3Config, LineConfig } from 'ember-d3-modifiers';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class ThresholdLinesDemoComponent extends Component {
-  d3Config = new D3Config({
-    dataConfig: {
-      'Temperature A': {
-        chartTypes: [
-          new LineConfig({ lineWidth: 3, className: 'temperature-a-custom-styling' })
-        ]
-      }
-    },
-    thresholds: [
-      { thresholdId: 'High Value Threshold', value: 5, className: 'high-value-threshold' },
-      { thresholdId: 'Low Value Threshold', value: 3, className: 'low-value-threshold' }
-    ]
-  });
+  @tracked d3Config;
+
+  @action
+  setupConfig() {
+    this.d3Config = new D3Config({
+      dataConfig: {
+        'Temperature A': {
+          chartTypes: [
+            new LineConfig({ lineWidth: 3, className: 'temperature-a-custom-styling' })
+          ]
+        }
+      },
+      thresholds: [
+        { thresholdId: 'High Value Threshold', value: 5, className: 'high-value-threshold' },
+        { thresholdId: 'Low Value Threshold', value: 3, className: 'low-value-threshold' }
+      ]
+    });
+    this.d3Config.startYaxisAtZero = true;
+  }
+
   @service fakeDataGenerator;
   get chartData() {
     return this.fakeDataGenerator.generateFakeTimeSeriesData({
