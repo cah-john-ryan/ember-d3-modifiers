93235656c972254844bf525a7ca3205e2bf0dcbf
John Ryan <john.ryan@cardinalhealth.com>
2020-08-07 08:10:19 -0400

    Creation of github actions for linting and testing

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,7 +2,7 @@
 
 const getChannelURL = require('ember-source-channel-url');
 
-module.exports = async function() {
+module.exports = async function () {
   return {
     scenarios: [
       {
@@ -45,19 +45,6 @@ module.exports = async function() {
           }
         }
       },
-<<<<<<< HEAD
-      // The default `.travis.yml` runs this scenario via `npm test`,
-      // not via `ember try`. It's still included here so that running
-      // `ember try:each` manually or from a customized CI config will run it
-      // along with all the other scenarios.
-      {
-        name: 'ember-default',
-        npm: {
-          devDependencies: {}
-        }
-      },
-=======
->>>>>>> 1f88d22... v3.16.0...v3.20.0
       {
         name: 'ember-default-with-jquery',
         env: {
